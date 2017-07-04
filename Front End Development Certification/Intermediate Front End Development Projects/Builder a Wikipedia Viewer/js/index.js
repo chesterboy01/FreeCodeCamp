b214281fd2fdc692a9f67d7e5bbc65c981a0eb02
chesterboy01 <chesterboy01@126.com>
2017-07-04 14:57:00 -0400

    find a way to call Wiki API successfully, and have done this project.

--- Front End Development Certification/Intermediate Front End Development Projects/Builder a Wikipedia Viewer/js/index.js
+++ Front End Development Certification/Intermediate Front End Development Projects/Builder a Wikipedia Viewer/js/index.js
@@ -1,17 +1,40 @@
-$("#random").on('click', function(){
-    window.open("https://en.wikipedia.org/wiki/Special:Random");  
-});
 
+$(document).ready(function() {
 
+  $("#random").on('click', function(){
+      window.open("https://en.wikipedia.org/wiki/Special:Random");  
+  });
 
+  var url = "https://en.wikipedia.org/w/api.php?callback=?";
 
-$("#sc").on('click', function(){
-    var str = $("#query").val();//获取文本框的文字内容
-    //alert(str);
-    var url = "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json";
-    //$("#entries li:nth-child(2)").html("1111111");//获取孩子对象 修改其属性
-    $.getJSON(url, function(data) {
-      //https://www.freecodecamp.com/challenges/target-a-specific-child-of-an-element-using-jquery
+  $("#sc").on('click', function(){
+      //getJSON is a shorthand Ajax function
+      $.ajax({
+        dataType: "jsonp",//still confused about json and jsonp
+        url:url,
+        data:{
+          action: 'query',
+          format: 'json',
+          formatversion: 2,
+          generator: 'search',
+          gsrsearch: $("#query").val(),//获取文本框的文字内容
+          gsrwhat: "text",
+          prop: "extracts|info",
+          exsentences: 3,      
+          exlimit: 10,
+          exintro: 1,
+          inprop: "url",
+        },
+        success: function(data){
+          //console.log("!!!!!!!");
+          data.query.pages.forEach(displayResult)//没有分号！
+        }
+      });
+  });
+
+  function displayResult(entries){
+        $("#entries").append("<li><a href="+entries.fullurl+">"+"<h2>"+entries.title+"</h2>"+entries.extract+"</a></li>");
+  }
+});   //https://www.freecodecamp.com/challenges/target-a-specific-child-of-an-element-using-jquery
       //https://www.freecodecamp.com/challenges/create-a-bulleted-unordered-list
-    });
-});
+      //$("#entries li:nth-child(2)").html("1111111");//获取孩子对象 修改其属性
